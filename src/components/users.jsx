d27fb34a33d081bb090da1e067aa2e53ea50c421
Anastasya <shamr44@mail.ru>
2022-12-03 00:39:47 +0300

    created table with information from mass

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
 import api from "../api";
-
-
+import "bootstrap/dist/css/bootstrap.css"
 
 const Users = () => {
     const [users, setUsers] = useState(api.users.fetchAll());
-
-    let count = 0;
+    
+    let count = users.length;
     const handleDelete = (userId) => {
 
     };
@@ -14,37 +13,44 @@ const Users = () => {
 
     };
 
-    const renderTable = () => {
-        return (
-            for (let i = 0; i <= users.length; i++){
-                <tr>
-                    <td>{users.name}</td>
-                    <td>{users.qualities}</td>
-                    <td>{users.profession}</td>
-                    <td>{users.completedMeetings}</td>
-                    <td>{users.rate}</td>
-                    <td><button></button></td>
-                </tr>
-            }
-        ); 
-    };
-
     return (
         <>
             <h1>{count} человек тусанет с тобой сегодня</h1>
-            <table className="table">
+            <table className="table table-striped">
                 <thead>
                     <tr>
                         <th scope="col">Имя</th>
                         <th scope="col">Качества</th>
                         <th scope="col">Проффесия</th>
-                        <th scope="col">Встретился, раз</th>
+                        <th scope="col">Встретился,раз</th>
                         <th scope="col">Оценка</th>
-                        <th scope="col"></th>
+                        <th scope="col"><></></th>
                     </tr>
                 </thead>
                 <tbody>
-                    {renderTable()};
+                    {users.map((user) => {
+                        return(
+                            <tr key={user._id}>
+                                <td>{user.name}</td>
+                                <td>
+                                    {user.qualities.map((item)=>
+                                        <span 
+                                            key={item._id}>
+                                                {item.name}
+                                        </span>
+                                    )}
+                                </td>
+                                <td>
+                                    {user.profession.name}
+                                </td>
+                                <td>{user.completedMeetings}</td>
+                                <td>{user.rate}</td>
+                                <td>
+                                    <button>Delete</button>
+                                </td>
+                            </tr>
+                        );
+                    })};
                 </tbody>
             </table>
         </>
